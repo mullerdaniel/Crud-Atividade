fdf51678acaab8d2ffb2c7c460e2e7cd509eee3a
Daniel Müller <miladaniel370@gmail.com>
2026-04-10 14:11:32 -0300

    Feat: Adicionando Estruturação base SpringBoot

---
+++ src/main/java/com/weg/Atividade_Crud/AtividadeCrudApplication.java
@@ -0,0 +1,13 @@
+package com.weg.Atividade_Crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weg/Atividade_Crud/AtividadeCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.Atividade_Crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
